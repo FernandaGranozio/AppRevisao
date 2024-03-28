423c06494eaff0d77c62958df6ff4066a851a191
Fernanda <fernandagranoziocastro@gmail.com>
2024-03-28 16:50:11 -0300

    Fim do Projeto revisão

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  n1='';
+  n2='';
+  nfinal=0;
+
+  constructor(private alertController: AlertController) {}
+
+  async AlertaDivisao0() {
+    const alert = await this.alertController.create({
+      header: 'Ops...',
+      subHeader: 'Esta operação não existe.',
+      message: 'Você está tentando fazer uma divisão por 0, porém está operação não existe.',
+      buttons: ['Ok'],
+    });
+    alert.present();
+  }
+
+
+  somar(){
+this.nfinal=parseFloat(this.n1)+parseFloat(this.n2);
+  }
+
+  subtrair(){
+    this.nfinal=parseFloat(this.n1)-parseFloat(this.n2)
+  }
+
+  multiplicar(){
+    this.nfinal=parseFloat(this.n1)*parseFloat(this.n2)
+  }
+
+  dividir(){
+    if (parseFloat(this.n2)==0) {
+
+      this.nfinal=0;
+      this.AlertaDivisao0();
+    //cria-se aqui a logica para falar que não existe divisão por 0//
+
+    } else {
+
+      this.nfinal=parseFloat(this.n1)/parseFloat(this.n2)
+    }
+  }
 
 }
